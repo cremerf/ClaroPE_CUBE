4fcf49aad02708fb0094fbdcc0ca53d1221e3aa1
andresdeinnocentiis <andresdeinnocentiis@gmail.com>
2022-09-01 17:01:11 -0300

    ClaroPath: OK

--- packages/claro_paths.py
+++ packages/claro_paths.py
@@ -3,4 +3,14 @@ import os
 
 class ClaroPath():
     def __init__(self) -> None:
-        self.BASE_DIR = 
+        # Directorio base de la carpeta del proyecto. La que contiene a todos los archivos:
+        self.BASE_DIR = Path(__file__).resolve().parent.parent 
+        
+        # Directorio de las credenciales:
+        self.CREDENTIALS_DIR = os.path.join(self.BASE_DIR, 'credentials.json')
+        
+        # Directorios de donde toma los datos:
+        # Por ahora no es necesario
+        
+        # Directorio de subida de datos:
+
