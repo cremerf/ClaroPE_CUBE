e64c49082e6a21a727dc1895212dffe3eea615fb
Federico Cremer <101053067+cremerf@users.noreply.github.com>
2022-09-05 17:12:26 -0300

    paths & buckets

--- packages/claro_paths.py
+++ packages/claro_paths.py
@@ -1,6 +1,36 @@
 from pathlib import Path
+from google.cloud import storage
 import os
 
 class ClaroPath():
-    def __init__(self) -> None:
-        self.BASE_DIR = 
+    def __init__(self, bucket_name: str) -> None:
+
+        self.bucket_name = bucket_name
+        self.table_name = ""
+
+        # Directorio base de la carpeta del proyecto. La que contiene a todos los archivos
+
+        self.BASE_DIR = Path(__file__).resolve().parent.parent 
+
+        # Directorio de las credenciales:
+
+        self.CREDENTIALS_DIR = os.path.join(self.BASE_DIR, 'credentials.json')
+
+        self.client = storage.Client(credentials= self.CREDENTIALS_DIR)
+
+        self.BUCKET_DIR = self.client.get_bucket(bucket_or_name= bucket_name)
+
+
+    def set_bucket_name(self, new_bucket_name) -> None:
+
+        self.bucket_name = new_bucket_name
+
+    def set_table_name(self, new_table_name):
+
+        self.new_table_name = new_table_name
+
+
+
+
+        
+
