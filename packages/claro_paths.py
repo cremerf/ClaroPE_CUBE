0da8e610a2ce15ce4fb91a6993b2bd7dc444dcb8
Federico Cremer <101053067+cremerf@users.noreply.github.com>
2022-09-06 11:06:44 -0300

    buckets & folders

--- packages/claro_paths.py
+++ packages/claro_paths.py
@@ -3,14 +3,13 @@ from google.cloud import storage
 import os
 
 class ClaroPath():
-    def __init__(self, bucket_name: str, working_dir: str) -> None:
+    def __init__(self, bucket_name: str, working_dir_name: str) -> None:
 
         self.bucket_name = bucket_name
         self.dataset_name = ""
         self.table_name = ""
-        self.working_dir = working_dir
 
-        # Directorio base de la carpeta del proyecto. La que contiene a todos los archivos
+        # Directorio base de la carpeta del proyecto. La que contiene a todos los archivos.
 
         self.BASE_DIR = Path(__file__).resolve().parent.parent 
 
@@ -18,9 +17,18 @@ class ClaroPath():
 
         self.CREDENTIALS_DIR = os.path.join(self.BASE_DIR, 'credentials.json')
 
+        # Conexion a Google Cloud Storage.
+
         self.client = storage.Client.from_service_account_json(json_credentials_path = self.CREDENTIALS_DIR)
 
+        # Seteo del bucket: bucket donde se guardaran los archivos.
+
         self.BUCKET = self.set_bucket()
+
+        # Nombre del directorio(folder/carpeta) dentro del bucket.
+
+        self.WORKING_DIR_NAME = working_dir_name
+
 
     def set_bucket(self):
 
